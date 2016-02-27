2dfb6b49e348cf1bb88586ac7c53e526be70f3b5
hydroboy407 <MacBookPro@Charleys-MacBook-Pro.local>
2016-02-27 18:07:12 -0500

    added the h files

---
+++ Project 2/Employee.h
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/* 
+ * File:   Employee.h
+ * Author: MacBookPro
+ *
+ * Created on February 27, 2016, 6:06 PM
+ */
+
+#ifndef EMPLOYEE_H
+#define EMPLOYEE_H
+
+
+
+#endif /* EMPLOYEE_H */
+
---
+++ Project 2/EmployeeDatabase.h
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/* 
+ * File:   EmployeeDatabase.h
+ * Author: MacBookPro
+ *
+ * Created on February 27, 2016, 6:06 PM
+ */
+
+#ifndef EMPLOYEEDATABASE_H
+#define EMPLOYEEDATABASE_H
+
+
+
+#endif /* EMPLOYEEDATABASE_H */
+
