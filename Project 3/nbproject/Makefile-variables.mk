7c7836c105fa8bbe089f0547eb5436087a248dbe
hydroboy407 <MacBookPro@Charleys-MacBook-Pro.local>
2016-03-20 17:10:51 -0400

    Start of project 3, some random project 2 stuff...

--- Project 3/nbproject/Makefile-variables.mk
+++ Project 3/nbproject/Makefile-variables.mk
@@ -9,19 +9,19 @@ CND_DISTDIR=dist
 # Debug configuration
 CND_PLATFORM_Debug=GNU-MacOSX
 CND_ARTIFACT_DIR_Debug=dist/Debug/GNU-MacOSX
-CND_ARTIFACT_NAME_Debug=project_2
-CND_ARTIFACT_PATH_Debug=dist/Debug/GNU-MacOSX/project_2
+CND_ARTIFACT_NAME_Debug=project_3
+CND_ARTIFACT_PATH_Debug=dist/Debug/GNU-MacOSX/project_3
 CND_PACKAGE_DIR_Debug=dist/Debug/GNU-MacOSX/package
-CND_PACKAGE_NAME_Debug=project2.tar
-CND_PACKAGE_PATH_Debug=dist/Debug/GNU-MacOSX/package/project2.tar
+CND_PACKAGE_NAME_Debug=project3.tar
+CND_PACKAGE_PATH_Debug=dist/Debug/GNU-MacOSX/package/project3.tar
 # Release configuration
-CND_PLATFORM_Release=MinGW-Windows
-CND_ARTIFACT_DIR_Release=dist/Release/MinGW-Windows
-CND_ARTIFACT_NAME_Release=project_2
-CND_ARTIFACT_PATH_Release=dist/Release/MinGW-Windows/project_2
-CND_PACKAGE_DIR_Release=dist/Release/MinGW-Windows/package
-CND_PACKAGE_NAME_Release=project2.tar
-CND_PACKAGE_PATH_Release=dist/Release/MinGW-Windows/package/project2.tar
+CND_PLATFORM_Release=GNU-MacOSX
+CND_ARTIFACT_DIR_Release=dist/Release/GNU-MacOSX
+CND_ARTIFACT_NAME_Release=project_3
+CND_ARTIFACT_PATH_Release=dist/Release/GNU-MacOSX/project_3
+CND_PACKAGE_DIR_Release=dist/Release/GNU-MacOSX/package
+CND_PACKAGE_NAME_Release=project3.tar
+CND_PACKAGE_PATH_Release=dist/Release/GNU-MacOSX/package/project3.tar
 #
 # include compiler specific variables
 #
--- Project 3/nbproject/Package-Release.bash
+++ Project 3/nbproject/Package-Release.bash
@@ -7,15 +7,15 @@
 # Macros
 TOP=`pwd`
 CND_PLATFORM=GNU-MacOSX
-CND_CONF=Debug
+CND_CONF=Release
 CND_DISTDIR=dist
 CND_BUILDDIR=build
 CND_DLIB_EXT=dylib
 NBTMPDIR=${CND_BUILDDIR}/${CND_CONF}/${CND_PLATFORM}/tmp-packaging
 TMPDIRNAME=tmp-packaging
-OUTPUT_PATH=${CND_DISTDIR}/${CND_CONF}/${CND_PLATFORM}/project_2
-OUTPUT_BASENAME=project_2
-PACKAGE_TOP_DIR=project2/
+OUTPUT_PATH=${CND_DISTDIR}/${CND_CONF}/${CND_PLATFORM}/project_3
+OUTPUT_BASENAME=project_3
+PACKAGE_TOP_DIR=project3/
 
 # Functions
 function checkReturnCode
@@ -60,15 +60,15 @@ mkdir -p ${NBTMPDIR}
 
 # Copy files and create directories and links
 cd "${TOP}"
-makeDirectory "${NBTMPDIR}/project2/bin"
+makeDirectory "${NBTMPDIR}/project3/bin"
 copyFileToTmpDir "${OUTPUT_PATH}" "${NBTMPDIR}/${PACKAGE_TOP_DIR}bin/${OUTPUT_BASENAME}" 0755
 
 
 # Generate tar file
 cd "${TOP}"
-rm -f ${CND_DISTDIR}/${CND_CONF}/${CND_PLATFORM}/package/project2.tar
+rm -f ${CND_DISTDIR}/${CND_CONF}/${CND_PLATFORM}/package/project3.tar
 cd ${NBTMPDIR}
-tar -vcf ../../../../${CND_DISTDIR}/${CND_CONF}/${CND_PLATFORM}/package/project2.tar *
+tar -vcf ../../../../${CND_DISTDIR}/${CND_CONF}/${CND_PLATFORM}/package/project3.tar *
 checkReturnCode
 
 # Cleanup
