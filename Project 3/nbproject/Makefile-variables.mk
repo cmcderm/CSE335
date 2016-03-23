f6202fc722f64d0e884dc55328e6086cbb3857cf
hydroboy407 <MacBookPro@host-100919.dhcp.egr.msu.edu>
2016-03-23 12:45:11 -0400

    lol stuff I don't know

--- Project 3/nbproject/Makefile-variables.mk
+++ Project 3/nbproject/Makefile-variables.mk
@@ -7,13 +7,13 @@ CND_BASEDIR=`pwd`
 CND_BUILDDIR=build
 CND_DISTDIR=dist
 # Debug configuration
-CND_PLATFORM_Debug=MinGW-Windows
-CND_ARTIFACT_DIR_Debug=dist/Debug/MinGW-Windows
+CND_PLATFORM_Debug=GNU-MacOSX
+CND_ARTIFACT_DIR_Debug=dist/Debug/GNU-MacOSX
 CND_ARTIFACT_NAME_Debug=project_3
-CND_ARTIFACT_PATH_Debug=dist/Debug/MinGW-Windows/project_3
-CND_PACKAGE_DIR_Debug=dist/Debug/MinGW-Windows/package
+CND_ARTIFACT_PATH_Debug=dist/Debug/GNU-MacOSX/project_3
+CND_PACKAGE_DIR_Debug=dist/Debug/GNU-MacOSX/package
 CND_PACKAGE_NAME_Debug=project3.tar
-CND_PACKAGE_PATH_Debug=dist/Debug/MinGW-Windows/package/project3.tar
+CND_PACKAGE_PATH_Debug=dist/Debug/GNU-MacOSX/package/project3.tar
 # Release configuration
 CND_PLATFORM_Release=GNU-MacOSX
 CND_ARTIFACT_DIR_Release=dist/Release/GNU-MacOSX
