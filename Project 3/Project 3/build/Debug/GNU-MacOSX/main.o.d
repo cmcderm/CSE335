0b91263b1a2905e21184b46c9935f434a4d0af52
hydroboy407 <MacBookPro@Charleys-MacBook-Pro.local>
2016-03-23 23:06:08 -0400

    Readme
    You’ll need to re-path the project.

---
+++ Project 3/Project 3/build/Debug/GNU-MacOSX/main.o.d
@@ -0,0 +1,18 @@
+build/Debug/GNU-MacOSX/main.o: main.cpp Employee.h Unit.h Visitor.h \
+  Manager.h PrintVisitor.h Department.h Group.h SumsalaryVisitor.h
+
+Employee.h:
+
+Unit.h:
+
+Visitor.h:
+
+Manager.h:
+
+PrintVisitor.h:
+
+Department.h:
+
+Group.h:
+
+SumsalaryVisitor.h:
---
+++ Project 3/Readme.txt
@@ -0,0 +1,18 @@
+heilcha1: Charles Heil
+
+Contributions:
+Charles Heil:
+readme.txt
+print functions
+setup
+Visitor Pattern
+debugging
+
+
+mcderm60: Connor McDermott
+
+Contributions:
+UML Chart;
+editting
+Visitor Pattern
+copy constructor;
