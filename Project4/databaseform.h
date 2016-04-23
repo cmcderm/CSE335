38089fd6e66266416c0395dff879468582964e19
loligator <loligator704@gmail.com>
2016-04-22 21:51:19 -0400

    Revert "Revert "Today""
    This reverts commit 8704d914302ca18f7407476f36994b60db3b2874.

--- Project4/databaseform.h
+++ Project4/databaseform.h
@@ -1,26 +1,50 @@
-#ifndef DATABASEFORM_H
-#define DATABASEFORM_H
+#include "databaseform.h"
+#include "ui_databaseform.h"
+#include "addrecordform.h"
+#include <QStandardItem>
+#include <QTableView>
+#include <QTableWidgetItem>
+#include <QTableWidget>
+#include <iostream>
 
-#include <QMainWindow>
 
-namespace Ui {
-class DatabaseForm;
+DatabaseForm::DatabaseForm(QWidget *parent) :
+    QMainWindow(parent),
+    ui(new Ui::DatabaseForm)
+{
+    ui->setupUi(this);
+    rowNumber=0;
+    ui->tableWidget->setSortingEnabled(true);
+
+
 }
 
-class DatabaseForm : public QMainWindow
+DatabaseForm::~DatabaseForm()
 {
-    Q_OBJECT
+    //connect(ui->tableWidget->insertRow();
+    delete ui;
+}
 
-public:
-    explicit DatabaseForm(QWidget *parent = 0);
-    ~DatabaseForm();
-    //stuuf
+void DatabaseForm::on_addRButton_clicked()
+{
 
-public slots:
-    void on_addRButton_clicked();
+    rowNumber++;
+    addRecordForm* arf = new addRecordForm();
+    arf->show();
+    ui->tableWidget->setRowCount(rowNumber);
+    //connect(arf,SIGNAL(retStr()),ui->tableWidget,SLOT(insertRow(int)));
+    connect(ui->tableWidget,SIGNAL(itemChanged(QTableWidgetItem*)),arf,SLOT(on_pushButtonAdd_clicked()));
+    //connect(arf,SIGNAL(observerDeleted()),ui->tableWidget,SLOT(insertRow(int)));
+     //ui->tableView->setModel(model);
+}
 
-private:
-    Ui::DatabaseForm *ui;
-};
 
-#endif // DATABASEFORM_H
+void DatabaseForm::accept_f_name(){
+    QTableWidgetItem *first =new QTableWidgetItem(QString("fnam"));
+    ui->tableWidget->setRowCount(rowNumber);
+    ui->tableWidget->setItem(rowNumber,0,first);
+    std::cout<<"gere2"<<std::endl;
+
+    //ui->tableWidget->setCurrentCell(1,1);
+
+}
--- Project4_fix/databaseform.cpp
+++ Project4_fix/databaseform.cpp
@@ -1,17 +1,27 @@
 #include "databaseform.h"
 #include "ui_databaseform.h"
 #include "addrecordform.h"
+#include <QStandardItem>
+
 
 DatabaseForm::DatabaseForm(QWidget *parent) :
     QMainWindow(parent),
     ui(new Ui::DatabaseForm)
 {
     ui->setupUi(this);
-    //ui->tableView->col
+    QStandardItemModel *model = new QStandardItemModel(2,4,this);
+   model->setHorizontalHeaderItem(0, new QStandardItem(QString("First Name")));
+   model->setHorizontalHeaderItem(1, new QStandardItem(QString("Last Name")));
+    model->setHorizontalHeaderItem(2, new QStandardItem(QString("Salary")));
+    model->setHorizontalHeaderItem(2, new QStandardItem(QString("Hire Year")));
+
+   ui->tableView->setModel(model);
 }
 
 DatabaseForm::~DatabaseForm()
 {
+    ui->tableWidget->insertColumn(1);
+    //connect(ui->tableWidget->insertRow();
     delete ui;
 }
 
@@ -19,4 +29,79 @@ void DatabaseForm::on_addRButton_clicked()
 {
     addRecordForm* arf = new addRecordForm();
     arf->show();
+}
+void DatabaseForm::newRow(){
+    ui->tableWidget->insertRow(1);
+}
+
+/*
+void DatabaseForm::accept_f_name(){
+    ui->tableWidget->setCurrentCell(1,1);
+
+}
+*/
+/////////
+#include "databaseform.h"
+#include "ui_databaseform.h"
+#include "addrecordform.h"
+#include <QStandardItem>
+#include <QTableView>
+#include <QTableWidgetItem>
+#include <QTableWidget>
+
+
+DatabaseForm::DatabaseForm(QWidget *parent) :
+    QMainWindow(parent),
+    ui(new Ui::DatabaseForm)
+{
+    ui->setupUi(this);
+    rowNumber=0;
+    QStandardItemModel *model = new QStandardItemModel(rowNumber,4,this);
+    model->setHorizontalHeaderItem(0, new QStandardItem(QString("First Name")));
+    model->setHorizontalHeaderItem(1, new QStandardItem(QString("Last Name")));
+    model->setHorizontalHeaderItem(2, new QStandardItem(QString("Salary")));
+    model->setHorizontalHeaderItem(3, new QStandardItem(QString("Hire Year")));
+
+   ui->tableView->setModel(model);
+
+
+}
+
+DatabaseForm::~DatabaseForm()
+{
+    //connect(ui->tableWidget->insertRow();
+    delete ui;
+}
+
+void DatabaseForm::on_addRButton_clicked()
+{
+    rowNumber++;
+    QStandardItem *newRow = new QStandardItem(QString("yo"));
+    QStandardItemModel *model = new QStandardItemModel(rowNumber,4,this);
+    addRecordForm* arf = new addRecordForm();
+    arf->show();
+     QTableWidgetItem *first =new QTableWidgetItem(QString("sdfdsf"));
+     ui->tableWidget->insertRow(rowNumber);
+     ui->tableWidget->setItem(rowNumber,0,first);
+    connect(ui->tableWidget,SIGNAL(itemChanged(QTableWidgetItem*)),this,SLOT(accept_f_name(QString)));
+     //ui->tableView->setModel(model);
+}
+/*
+void DatabaseForm::newRow(){
+    ui->tableWidget->insertRow(1);
+}
+*/
+
+void DatabaseForm::accept_f_name(QString fname){
+    //rowNumber++;
+     //QStandardItemModel *model = new QStandardItemModel(rowNumber,4,this);
+    //QStandardItem *newRow = new QStandardItem(QString(fname));
+    QTableWidgetItem *first =new QTableWidgetItem(QString(fname));
+   // model->setItem(0,rowNumber,newRow);
+    //ui->tableView->setModel((model));
+    ui->tableWidget->insertRow(rowNumber);
+    ui->tableWidget->setItem(rowNumber,0,first);
+
+    //ui->tableWidget->setCurrentCell(1,1);
+
 }
